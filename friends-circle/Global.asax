7acb26480c535f9730abf2e3a1e2afa072f9b3e9
Jujunol <jujunol@outlook.com>
2016-08-27 14:07:47 -0400

    Initial commit - updated nuget packages, added font-awesome

---
+++ friends-circle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="friends_circle.MvcApplication" Language="C#" %>
---
+++ friends-circle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(friends_circle.Startup))]
+namespace friends_circle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
